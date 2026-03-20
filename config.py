5798eee0e819b757f843191d9c84497cb5d1c4fb
sloubrie <sloubrie@ucsd.edu>
2026-03-20 14:49:20 -0400

    Fix export folder

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ import os
 REDCAP_API_URL = "https://redcap.ucsd.edu/api/"
 REDCAP_API_TOKEN = os.getenv("REDCAP_TOKEN")
 
-RAW_CSV_PATH = "data/raw_redcap_export.csv"
-LESIONS_LONG_PATH = "data/lesions_long.csv"
-VALIDATION_REPORT_PATH = "data/validation_report.txt"
+RAW_CSV_PATH = "/space/wil-syn01/1/cmig_body/RSIData/Breast/UCSD/Multiband/metadata/raw_redcap_export.csv"
+LESIONS_LONG_PATH = "/space/wil-syn01/1/cmig_body/RSIData/Breast/UCSD/Multiband/metadata/breast_database.csv"
+VALIDATION_REPORT_PATH = "/space/wil-syn01/1/cmig_body/RSIData/Breast/UCSD/Multiband/validation_report.txt"
 
 META_COLS = [
     "redcap_event_name",
